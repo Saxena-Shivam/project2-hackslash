3afb48e031499cbeae8d207e015f59309088bc54
Shivam Saxena <shivamsaxena862006@gmail.com>
2024-02-29 22:50:12 +0530

    styles are updated

--- index.js
+++ index.js
@@ -24,3 +24,79 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+document.addEventListener("keypress", function(event) {
+
+  perform(event.key);
+
+  buttonAnimation(event.key);
+
+});
+function perform(key) {
+
+  switch (key) {
+    case "37":
+    
+      break;
+
+    case "39":
+
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let ball = document.getElementById("ballID");
+//   var myVar = setInterval(spostaDiv, 90);
+//   var margin = 0;
+//
+//   let l = window.screen.width;
+//   let w = 1300;
+//
+//   function spostaDiv() {
+//     if (margin >= l) {
+//       margin = -w;
+//     }
+//     else {
+//       ball.style.marginLeft = margin + "px";
+//     }
+//         margin -= 10;
+//   }
+// let ball = document.getElementById("ballID");
+//
+//       var myVar = setInterval(spostaDiv, 90);
+//       var margin = 0;
+//
+//       let l = window.screen.width;
+//       let w = 1300;
+//
+//       function spostaDiv() {
+//           console.log(w);
+//
+//       }
